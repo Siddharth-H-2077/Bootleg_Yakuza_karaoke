92548013721acdf5c106d38f3177ecd16ec858f9
brokensilver75 <rsmadhan75@gmail.com>
2023-08-21 11:05:05 +0530

    Started Lane

--- Bootleg Karaoke/Assets/Scripts/Note.cs
+++ Bootleg Karaoke/Assets/Scripts/Note.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class Note : MonoBehaviour
 {
+    double timeInstantiated;
+    public float assignedTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeInstantiated = SongManager.GetaudioSourceTime();        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        double timeSinceInstatiated = SongManager.GetaudioSourceTime() - timeInstantiated;
+        float t = (float)(timeSinceInstatiated / (SongManager.songManagerInstance.noteTime * 2));
+
+        if (t > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(Vector3.right * SongManager.songManagerInstance.noteSpawnX, Vector3.right * SongManager.songManagerInstance.noteDespawnX, t);
+        }
     }
 }
  
